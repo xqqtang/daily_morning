55de014608914ede069cbc0f0eb59bb73b0da3e3
xqqtang <125367276+xqqtang@users.noreply.github.com>
2023-02-21 06:54:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+
 from datetime import date, datetime
 import math
 from wechatpy import WeChatClient
@@ -7,6 +7,7 @@ import requests
 import os
 import random
 
+'''
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
