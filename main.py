68fb0f47883dd02e1dc7f65ad724ca9896ad9b9d
xqqtang <125367276+xqqtang@users.noreply.github.com>
2023-02-21 05:55:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_birthday():
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
-'''
+
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
@@ -54,3 +54,62 @@ wm = WeChatMessage(client)
 data = {"weather":{"value":21},"temperature":{"value":22},"love_days":{"value":23},"birthday_left":{"value":24},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
+'''
+
+class WechatMessagePush:
+    def __init__(self, appid, appsecret, temple_id):
+        self.appid = appid
+        self.appsecret = appsecret
+
+        # 模板id,参考公众号后面的模板消息接口 -> 模板ID(用于接口调用):IG1Kwxxxx
+        self.temple_id = temple_id
+
+        self.token = self.get_Wechat_access_token()
+   
+
+    def get_Wechat_access_token(self):
+        '''
+        获取微信的access_token： 获取调用接口凭证
+        :return:
+        '''
+        url = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={self.appid}&secret={self.appsecret}"
+        response = requests.get(url)
+
+        res = response.json()
+        if "access_token" in res:
+            token = res["access_token"]
+            return token
+
+    def get_wechat_accout_fans_count(self):
+        '''
+        获取微信公众号所有粉丝的openid
+        '''
+        next_openid = ''
+        url = f"https://api.weixin.qq.com/cgi-bin/user/get?access_token={self.token}&next_openid={next_openid}"
+        response = requests.get(url)
+        res = response.json()['data']['openid']
+
+    def send_wechat_temple_msg(self, content):
+        '''
+        发送微信公众号的模板消息'''
+        url = f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={self.token}"
+
+        fan_open_id = self.get_wechat_accout_fans_count()
+        for open_id in fan_open_id:
+            body = {
+                "touser": open_id,
+                'template_id': self.temple_id,    
+                # 'url': 'http://www.jb51.net',  
+                "topcolor": "#667F00",
+                "data": {
+                    "content": {"value": content}
+                }
+            }
+            headers = {"Content-type": "application/json"}
+            data = json.JSONEncoder().encode(body)
+            res = requests.post(url=url, data=data, headers=headers)
+            
+if __name__ == '__main__':
+
+    WechatMessagePush(app_id, app_secret, template_id).send_wechat_txt_msg(msg="测试")           
+            
